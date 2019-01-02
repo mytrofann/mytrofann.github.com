e192f8e771b62395eaa5121cca7fd5102a2e7049
Dmitriy Kravchuk <mytrofann@gmail.com>
2019-01-02 15:46:28 +0200

    Add files via upload

---
+++ Seacruises/js/javascript.js
@@ -0,0 +1,76 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = navMain.querySelector('.main-nav__toggle');
+var mainNavWrapper = navMain.querySelector('.main-nav__wrapper');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navMain.classList.add('main-nav--js');
+    navMain.classList.remove('main-nav--nojs');
+    mainNavWrapper.style.display = 'none';
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--js');
+    navMain.classList.remove('main-nav--nojs');
+    mainNavWrapper.style.display = 'block';
+  }
+});
+
+var link = document.querySelector(".offer__button");
+var popup = document.querySelector(".modal-login");
+var close = popup.querySelector(".modal__close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
